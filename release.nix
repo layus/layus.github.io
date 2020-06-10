be697f2051f274709b958ec7ab85c7ddfaff8906
Guillaume Maudoux <layus.on@gmail.com>
2020-06-10 17:52:48 +0200

    Init hakyll site

---
+++ release.nix
@@ -0,0 +1,57 @@
+let
+  sources = import ./nix/sources.nix;
+in
+{ compiler ? "ghc883"
+, pkgs ? import sources.nixpkgs { }
+}:
+
+let
+  inherit (pkgs.lib.trivial) flip pipe;
+  inherit (pkgs.haskell.lib) appendPatch appendConfigureFlags;
+
+  haskellPackages = pkgs.haskell.packages.${compiler}.override {
+    overrides = hpNew: hpOld: {
+      hakyll =
+        pipe
+           hpOld.hakyll
+           [ (flip appendPatch ./hakyll.patch)
+             (flip appendConfigureFlags [ "-f" "watchServer" "-f" "previewServer" ])
+           ];
+
+      #blog = hpNew.callCabal2nix "blog" ./. { };
+      blog = pkgs.haskell.packages.${compiler}.callPackage ({ mkDerivation, base, hakyll, stdenv }:
+        mkDerivation {
+          pname = "blog-hakyll";
+          version = "0.1.0.0";
+          src = ./.;
+          isLibrary = false;
+          isExecutable = true;
+          executableHaskellDepends = [ base hakyll ];
+          license = "unknown";
+          hydraPlatforms = stdenv.lib.platforms.none;
+        }
+      ) {};
+
+      niv = import sources.niv { };
+    };
+  };
+
+  project = haskellPackages.blog;
+in
+{
+  project = project;
+
+  shell = haskellPackages.shellFor {
+    packages = p: with p; [
+      project
+    ];
+    buildInputs = with haskellPackages; [
+      ghcid
+      hlint       # or ormolu
+      niv
+      pkgs.cacert # needed for niv
+      pkgs.nix    # needed for niv
+    ];
+    withHoogle = true;
+  };
+}
